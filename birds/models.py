853e836bb85c18f2ed1b04f120713b326d28cdc7
Sexeniya <user@sexeniya-MacBook-Pro.local>
2025-04-14 19:10:22 +0300

    Удаление игнорируемых файлов из репозитория

--- birds/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Bird(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='birds/')
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
--- birds/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('birds/', views.bird_list, name='bird_list'),
-    path('add/', views.add_bird, name='add_bird'),
-    path('quiz/', views.quiz, name='quiz'),
-]
